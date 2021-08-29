96fcffdb1a9880a508b9aed2758cf5f57c1df664
EthanChen0802 <u83kw4@gmail.com>
2021-08-29 23:24:56 +0800

    second

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,StickerSendMessage, LocationSendMessage
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('imh32RPdHsEOJHkvFtYiCBFSlOyQsC5T2prnBjeSMKp8KAytI4nXANJIGJtLMKnzjqtykK82Hnr26XhuqA2MbTStyjzWXlshpzSqMsHjgQmjwyM1AgQEJVS0KwMepctPBeAdPVSn8+burdPeCVE/OQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('773ffcc78591377eb80223fb1621b929')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    r = '很抱歉, 您說什麼'
+
+    if'給我貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='11537',
+            sticker_id='52002734'
+        )
+        
+
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message)
+        return
+
+    if '我現在在哪' in msg:
+        location_message = LocationSendMessage(
+        title='你的位置',
+        address='Singapore',
+        latitude=1.290270,
+        longitude=103.851959
+        )
+
+        line_bot_api.reply_message(
+        event.reply_token,
+        location_message)
+        return
+
+    if msg in ['hi', 'Hi']:
+        r = '嗨'
+    elif msg =='你吃飯了嗎':
+        r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif '訂位' in msg:
+        r = '您想訂位, 是嗎?'
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text= r))
+
+
+if __name__ == "__main__":
+    app.run()
